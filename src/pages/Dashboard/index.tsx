c7d3302284d9b9dbe5d89d46f5aecba61c95cbc1
Lucas Thomaz Ribeiro <lucasthomazr@hotmail.com>
2020-06-20 21:47:12 -0300

    Dashboard Estilizado

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImage from '../../assets/logo-github.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImage} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/60111139?s=460&v=4"
+            alt="Thomaz"
+          />
+          <div>
+            <strong>Thomaz201/gostack</strong>
+            <p>Repositório Curso Rocketseat</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
